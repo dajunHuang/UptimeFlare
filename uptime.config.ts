a3da6ba85a57ba7d0d21c6c1a1fc0045a8bc3fff
Dajun Huang <djhuang_1@qq.com>
2025-09-05 10:09:58 +0800

    Comment out maintenance configuration in uptime.config.ts
    Commented out the maintenance configuration for clarity.

--- uptime.config.ts
+++ uptime.config.ts
@@ -125,21 +125,21 @@ const workerConfig: WorkerConfig = {
 // Of course, you can leave it empty if you don't need this feature
 // const maintenances: MaintenanceConfig[] = []
 const maintenances: MaintenanceConfig[] = [
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    // monitors: ['foo_monitor', 'bar_monitor'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    // title: 'Test Maintenance',
-    // Description of the maintenance, will be shown at status page
-    // body: 'This is a test maintenance, server software upgrade',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // start: '2025-04-27T00:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    // end: '2025-04-30T00:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    // color: 'blue',
-  },
+  // {
+  //   [Optional] Monitor IDs to be affected by this maintenance
+  //   monitors: ['foo_monitor', 'bar_monitor'],
+  //   [Optional] default to "Scheduled Maintenance" if not specified
+  //   title: 'Test Maintenance',
+  //   Description of the maintenance, will be shown at status page
+  //   body: 'This is a test maintenance, server software upgrade',
+  //   Start time of the maintenance, in UNIX timestamp or ISO 8601 format
+  //   start: '2025-04-27T00:00:00+08:00',
+  //   [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+  //   if not specified, the maintenance will be considered as on-going
+  //   end: '2025-04-30T00:00:00+08:00',
+  //   [Optional] color of the maintenance alert at status page, default to "yellow"
+  //   color: 'blue',
+  // },
 ]
 
 // Don't forget this, otherwise compilation fails.
